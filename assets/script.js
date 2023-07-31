2be352a653e74aa47785c3cc917d5ad900ddacde
PsychoRSS <Bobgonzalez516@gmail.com>
2023-07-31 01:48:53 -0600

    working on api calls

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,41 @@
 var apiKey = "9ac7556bd3a00d2411501fa796cbd188";
-var city = "longmont";
-
+var city = "Longmont";
+var weatherEl = document.getElementById('#weather')
 
 
 
 function getWether() {
     // e.preventDefault
-    var api =  "https://api.openweathermap.org/data/2.5/forecast?q= "+ city + "&appid=" + apiKey
+    var api =  "https://api.openweathermap.org/data/2.5/forecast?q= "+ city + "&cnt=7&appid=" + apiKey
 
-    fetch(api, function(response) {
+    fetch(api)
+    .then(function(response) {
+        if(!response.ok) {
+            throw new Error("rawr")
+        }
         return response.json();
-    }) 
-    .then(function (response) {
-        var weatherData = response
+    }).then (function (data) {
+        var weatherData = data.list
+        // var newNewData = weatherData.weather
+    
+        for (let o = 0; o < weatherData.length; o++) {
+
+            const element = weatherData[o];
+            
+            const dayWeather = element.weather
+            var weatherELL = document.createElement('p');
+            weatherELL.textContent = dayWeather;
+
+            weatherEl.appendChild(weatherELL);
+            console.log(element.weather)
+            console.log(dayWeather)
+        }
+      
+ 
+        console.log(data)
         console.log(weatherData)
+        // console.log(newData)
+        // console.log(newNewData)
     })
 }
 
